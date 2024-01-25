62a14c066ffb569850f18e8663a5f865d60a10d2
dduneon <dduneon@gmail.com>
2024-01-25 11:12:32 +0900

    [INIT] Init Project

---
+++ src/main/java/com/nhnacademy/minidooray/gateway/MinidoorayGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidoorayGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinidoorayGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/minidooray/gateway/MinidoorayGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinidoorayGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
