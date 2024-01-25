d3edb825a4fdaea1b7cef2f7cabaf192da85306c
dduneon <dduneon@gmail.com>
2024-01-25 23:15:39 +0900

    [ADD] page add

--- src/main/java/com/nhnacademy/minidooray/gateway/controller/common/AccountRegisterController.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/controller/common/AccountRegisterController.java
@@ -5,12 +5,14 @@ import com.nhnacademy.minidooray.gateway.service.account.AccountClientService;
 import javax.validation.Valid;
 import javax.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/register")
 @RequiredArgsConstructor
@@ -28,8 +30,10 @@ public class AccountRegisterController {
       throw new ValidationException();
     //todo exception handling
 
-    if(accountClientService.createAccount(accountRegisterRequestDTO))
+    if(accountClientService.createAccount(accountRegisterRequestDTO)) {
+      log.debug("doRegister(): success create");
       return "redirect:/login";
+    }
 
     //todo do something when register failed
     return "redirect:/login";
