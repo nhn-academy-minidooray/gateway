7752d74893266559631c34c5469cff0862cff567
dduneon <dduneon@gmail.com>
2024-01-26 15:23:14 +0900

    [ADD] 나중을 위해 Status Enum class 추가

--- src/main/java/com/nhnacademy/minidooray/gateway/domain/account/Status.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/domain/account/Status.java
@@ -1,4 +1,16 @@
 package com.nhnacademy.minidooray.gateway.domain.account;
 
+import lombok.Getter;
+
+@Getter
 public enum Status {
+  JOIN("가입"),
+  DORMANT("휴면"),
+  WITHDRAWAL("탈퇴");
+
+  private final String value;
+
+  Status(String value){
+    this.value = value;
+  }
 }
