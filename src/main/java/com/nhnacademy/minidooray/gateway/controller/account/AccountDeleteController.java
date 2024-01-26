537a250967ea1937ab528389477a522501eab90c
dduneon <dduneon@gmail.com>
2024-01-26 15:21:19 +0900

    [FIX] 탈퇴쪽 log 수정

--- src/main/java/com/nhnacademy/minidooray/gateway/controller/account/AccountDeleteController.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/controller/account/AccountDeleteController.java
@@ -3,11 +3,13 @@ package com.nhnacademy.minidooray.gateway.controller.account;
 import com.nhnacademy.minidooray.gateway.domain.account.request.AccountInfoRequestDTO;
 import com.nhnacademy.minidooray.gateway.service.account.AccountClientService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+@Slf4j
 @Controller
 @RequestMapping("/delete")
 @RequiredArgsConstructor
@@ -15,12 +17,12 @@ public class AccountDeleteController {
   private final AccountClientService accountClientService;
   @DeleteMapping
   public String doDelete(@SessionAttribute(name = "USER_INFO") String accountId) {
-
+    //todo 탈퇴 진행
     if(accountClientService.deleteAccount(new AccountInfoRequestDTO(accountId))) {
-      // delete success
+      log.debug("doDelete(): delete success");
       return "redirect:/login";
     }
-    // todo when delete failed
+    log.debug("doDelete(): delete failed");
     return "redirect:/login";
   }
 }
