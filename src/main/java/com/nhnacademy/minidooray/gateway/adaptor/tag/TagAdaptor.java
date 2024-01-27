7bdf10985f811bd325e4580178ed49fa01691ceb
dduneon <dduneon@gmail.com>
2024-01-28 06:20:50 +0900

    [ADD] Detail 페이지 수정

--- src/main/java/com/nhnacademy/minidooray/gateway/adaptor/tag/TagAdaptor.java
+++ src/main/java/com/nhnacademy/minidooray/gateway/adaptor/tag/TagAdaptor.java
@@ -1,5 +1,15 @@
 package com.nhnacademy.minidooray.gateway.adaptor.tag;
 
-public interface TagAdaptor {
+import com.nhnacademy.minidooray.gateway.domain.tag.request.TagCreateRequestDTO;
+import com.nhnacademy.minidooray.gateway.domain.tag.request.TagDeleteRequestDTO;
+import com.nhnacademy.minidooray.gateway.domain.tag.request.TagInfoRequestDTO;
+import com.nhnacademy.minidooray.gateway.domain.tag.request.TagModifyRequestDTO;
+import com.nhnacademy.minidooray.gateway.domain.tag.response.TagInfoResponseDTO;
+import java.util.List;
 
+public interface TagAdaptor {
+  List<TagInfoResponseDTO> selectTagListInProject(TagInfoRequestDTO tagInfoRequestDTO);
+  boolean insertTagInProject(TagCreateRequestDTO tagCreateRequestDTO);
+  boolean updateTag(Long tagId, TagModifyRequestDTO tagModifyRequestDTO);
+  boolean deleteTag(TagDeleteRequestDTO tagDeleteRequestDTO);
 }
